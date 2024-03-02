cbc9a5542696a69364bb5b9becb55141004472e6
Muhammad Awan Ardy Firmansyah <muhammadawan46@gmail.com>
2024-03-03 04:51:25 +0800

    tes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ module.exports = {
       center: true,
       padding: "16px",
     },
+    colors: {
+      nogizaka: "#4a044e",
+    },
     extend: {
       colors: {
         primary: "#14b8a6",
         dark: "#0f172a",
         secondary: "#64748b",
+        nogizaka: "#4a044e",
       },
       screens: {
         "2xl": "1320px",
